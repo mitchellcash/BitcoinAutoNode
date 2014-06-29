a92b49dbcb87b0aa490a824bacf87ee4bdc66937
mitchellcash <mitchell.cash@gmail.com>
2014-06-29 12:16:09 +1000

    Update and rename bitcoinAutoNode.sh to Auto-Deploy-Full-Bitcoin-Node.sh

---
+++ Auto-Deploy-Full-Bitcoin-Node.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "### Change to home directory"
+cd ~
+echo "### Change your root password"
+sudo passwd
+echo "### Update Ubuntu"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install software-properties-common python-software-properties ufw -y
+echo "### Allow ports 22, 8333 and enable The Uncomplicated Firewall"
+sudo ufw allow 22/tcp
+sudo ufw allow 8333/tcp
+sudo ufw --force enable
+echo "### Creating Swap File"
+dd if=/dev/zero of=/swapfile bs=1M count=1024 ; mkswap /swapfile ; swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+echo "### Creating bitcoin.conf"
+mkdir ~/.bitcoin/
+config=".bitcoin/bitcoin.conf"
+touch $config
+echo "server=1" > $config
+echo "daemon=1" >> $config
+echo "maxconnections=50" >> $config
+echo "disablewallet=1" >> $config
+randUser=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
+randPass=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
+echo "rpcuser=$randUser" >> $config
+echo "rpcpassword=$randPass" >> $config
+echo "### Downloading Bitcoin Core 0.9.2.1"
+wget https://bitcoin.org/bin/0.9.2.1/bitcoin-0.9.2.1-linux.tar.gz
+echo "### Installing Bitcoin Core 0.9.2.1"
+rm -f -r ~/Bitcoin-Core/
+mkdir ~/Bitcoin-Core/
+tar -xvvzf bitcoin-0.9.2.1-linux.tar.gz --directory ~/Bitcoin-Core/ --strip-components 1
+rm -f -r bitcoin-0.9.2.1-linux.tar.gz
+echo "### Scheduling Cron Job to run Bitcoin Core on boot"
+(crontab -l ; echo "@reboot ~/Bitcoin-Core/bin/64/./bitcoind")| crontab -
+echo "### System will now reboot"
+reboot
